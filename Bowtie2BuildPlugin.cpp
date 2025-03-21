b417e5ff092b95130e84cf3937411807e2155d3b
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-03-21 13:18:29 -0400

    Added parameters

--- Bowtie2BuildPlugin.cpp
+++ Bowtie2BuildPlugin.cpp
@@ -12,9 +12,28 @@ void Bowtie2BuildPlugin::run() {}
 void Bowtie2BuildPlugin::output(std::string file) {
  std::string outputfile = file;
  std::string myCommand = "";
-myCommand +=  "bowtie2-build";
-myCommand += " -q ";
-myCommand += inputfile;
+myCommand +=  "bowtie2-build ";
+std::ifstream infile(inputfile, std::ios::in);
+std::string onefile; 
+   std::vector<std::string> myfiles;
+   while (!infile.eof()){
+   getline(infile, onefile);
+   if (onefile.length() != 0) {
+	   if (onefile[0] != '/')
+   myfiles.push_back(std::string(PluginManager::prefix())+std::string(onefile));
+	   else
+		   myfiles.push_back(std::string(onefile));
+   }
+
+}
+
+for (int i = 0; i < myfiles.size(); i++) {
+   myCommand += myfiles[i];
+   if (i != myfiles.size()-1)
+	   myCommand += ",";
+}
+//myCommand += " -q ";
+//myCommand += inputfile;
 myCommand += " ";
 myCommand += outputfile;
 std::cout << myCommand << std::endl;
